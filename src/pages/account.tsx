f6cc7944ec3b3a0d9719ee041de85c29da31e68f
HOWARD1021 <44057435+HOWARD1021@users.noreply.github.com>
2022-10-22 16:34:43 +0800

    authLogin

---
+++ src/pages/account.tsx
@@ -0,0 +1,35 @@
+import { getSession, signOut, useSession } from 'next-auth/react'
+import React from 'react'
+
+function account() {
+    const { data: session, status } = useSession();
+    if (status === 'authenticated') {
+        return (
+            <div>
+                <p>Welcome {session.user?.name}</p>
+                <button onClick={() => signOut()}>log out</button>
+            </div>
+        )
+    } else {
+        return (
+            <div>Account</div>
+        )
+
+    }
+}
+
+export default account
+
+export const getServerSideProps = async (context) =>{
+    const session =  await getSession(context)
+    if(!session){
+        return {
+            redirect:{
+                destination:'/login',
+            }
+        }        
+    }
+    return {
+        props:{session}
+    }
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from "next";
+import { useSession, signIn, signOut ,getSession } from "next-auth/react";
+import Image from 'next/image'
+const Login: NextPage = () => {
+
+  const { data: session, status } = useSession();
+  if (session) {
+    return (
+      <div>
+        Signed in as {session.user?.email} <br />
+        <Image src={session.user?.image}
+           height={100} width={100} />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
+export default Login
+
+export const getServerSideProps = async (context) =>{
+  const session =  await getSession(context)
+  if(session){
+      return {
+          redirect:{
+              destination:'/account',
+          }
+      }        
+  }
+  return {
+      props:{session}
+  }
+}
